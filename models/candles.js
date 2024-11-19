e735cff578bef171d52ad4fdb2effbae11cce4dc
nmartin <nate.martin27@gmail.com>
2024-11-19 17:35:20 -0500

    images

--- models/candles.js
+++ models/candles.js
@@ -3,6 +3,7 @@ const mongoose =require('mongoose')
 const candleSchema = new mongoose.Schema({
     name: String,
     hasPrize: Boolean,
+    candleImg: String,
 })
 
 const Candle = mongoose.model('Candle', candleSchema)
